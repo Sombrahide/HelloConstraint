52323e990362e5762b7f811bdb66799c4c9f5ecf
Sombrahide <47358363+Sombrahide@users.noreply.github.com>
2019-10-17 18:10:15 +0200

    La siguiente version del proyecto, ahora el boton contador aumenta el numero que sale en la etiqueta de texto y el boton toast pone la etiqueta de texto a cero.

--- app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.helloconstraint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public int _contador;
+    public TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sumarContador(View view){
+        _contador = _contador + 1;
+        texto = findViewById(R.id.show_count);
+        String aTexto = Integer.toString(_contador);
+        texto.setText(aTexto);
+    }
+    public void borrarContador(View view){
+        _contador = 0;
+        texto = findViewById(R.id.show_count);
+        String aTexto = Integer.toString(_contador);
+        texto.setText(aTexto);
     }
 }
